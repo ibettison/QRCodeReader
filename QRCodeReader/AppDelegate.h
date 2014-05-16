a4a6b8662bbca9776619a0f54aa1b51a52938ee2
ibettison <ian.bettison@ncl.ac.uk>
2014-05-16 13:00:54 +0100

    Remote test change

--- QRCodeReader/AppDelegate.h
+++ QRCodeReader/AppDelegate.h
@@ -4,7 +4,7 @@
 //
 //  Created by Ian Bettison on 14/05/2014.
 //  Copyright (c) 2014 Ian Bettison. All rights reserved.
-//  No so Local Change
+//  Remote User changed this
 
 #import <UIKit/UIKit.h>
 
